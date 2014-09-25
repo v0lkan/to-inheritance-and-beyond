8256f551d4dce2f3f5adbc6daf0f77a6cce33f89
Volkan Ozcelik <volkan.ozcelik@jivesoftware.com>
2014-09-25 13:12:40 -0700

    initial commit.

---
+++ temp.js
@@ -0,0 +1,5 @@
+console.log(Number.constructor === Function);
+console.log(Boolean.constructor === Function);
+console.log(Object.constructor === Function);
+console.log(Date.constructor === Function);
+console.log(String.constructor === Function);
